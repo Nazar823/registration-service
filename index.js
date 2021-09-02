48d60f0e02e1af985edb3b49c20aa5c7946b2bed
NazarExceedTeam <ignatenko.exceedteam@gmail.com>
2021-09-02 11:45:28 +0300

    fix: deledet not using code

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const PORT = 5001
-const bodyParser = require("body-parser");
 const apiRoutes = require('./src/Routes/Routes')
 const app = express()
 app.use(cors())
-app.use(bodyParser.json())
 app.use('/', apiRoutes)
 
 
